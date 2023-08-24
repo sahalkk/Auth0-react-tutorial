5726e5a25ef85e10efd76bd3ce015cf99fb40618
sahalkk <msahalkk357@gmail.com>
2023-08-24 15:20:35 +0530

    css added

--- auth0/src/components/Profile.js
+++ auth0/src/components/Profile.js
@@ -10,8 +10,8 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
+      <div className="profile">
+        <img className="photo" src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       </div>
